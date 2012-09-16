cd06ced43539dcbe573916aeee3a8416f7edba80
Doug Mason <d.mason82@gmail.com>
2012-09-16 02:57:10 -0700

    removed unneeded properties from class.

--- DWeather/DWeatherWeatherForecastDay.h
+++ DWeather/DWeatherWeatherForecastDay.h
@@ -11,24 +11,15 @@
  */
 
 /*
- JSONObject currentForecastDateObject = currentSimpleForecastObject.getJSONObject("date");
- newForecast.setIconPath(currentSimpleForecastObject.getString("icon_url"));
- newForecast.setCondition(currentSimpleForecastObject.getString("conditions"));
- newForecast.setDay(Integer.toString(currentForecastDateObject.getInt("month"))+"/"+Integer.toString(currentForecastDateObject.getInt("day"))+"/"+Integer.toString(currentForecastDateObject.getInt("year")) );
- newForecast.setHighTemp(Float.parseFloat(currentSimpleForecastObject.getJSONObject("high").getString("fahrenheit")));
- newForecast.setLowTemp(Float.parseFloat(currentSimpleForecastObject.getJSONObject("low").getString("fahrenheit")));
- newForecast.setDayOfWeek(currentForecastDateObject.getString("weekday_short"));
  */
 #import <Foundation/Foundation.h>
 
 @interface DWeatherWeatherForecastDay : NSObject
 @property(nonatomic,strong)NSNumber *lowTemp;
 @property(nonatomic,strong)NSNumber *highTemp;
-@property(nonatomic,strong)NSNumber *currentTemp;
 @property(nonatomic,strong)NSString *dayOfWeek;
 @property(nonatomic,strong)NSString *forecastDay;
 @property(nonatomic,strong)NSString *forecastIconPath;
 @property(nonatomic,strong)NSString *forecastCondition;
 @property(nonatomic,strong)NSString *forecastWindText;
-@property(nonatomic,strong)NSString *currentLocation;
 @end
