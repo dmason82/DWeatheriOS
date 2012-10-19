e2fa10d02b829e7c508b175003ec471e9f024a5a
Doug Mason <d.mason82@gmail.com>
2012-10-19 14:31:34 -0700

    Created new class for current weather conditions

---
+++ DWeather/DWeatherCurrentWeather.h
@@ -0,0 +1,13 @@
+//
+//  DWeatherCurrentWeather.h
+//  DWeather
+//
+//  Created by Doug Mason on 9/15/12.
+//  Copyright (c) 2012 Doug Mason. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DWeatherCurrentWeather : NSObject
+
+@end
---
+++ DWeather/DWeatherCurrentWeather.m
@@ -0,0 +1,13 @@
+//
+//  DWeatherCurrentWeather.m
+//  DWeather
+//
+//  Created by Doug Mason on 9/15/12.
+//  Copyright (c) 2012 Doug Mason. All rights reserved.
+//
+
+#import "DWeatherCurrentWeather.h"
+
+@implementation DWeatherCurrentWeather
+
+@end
